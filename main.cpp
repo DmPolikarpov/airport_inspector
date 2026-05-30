26b12386c6af27783ad6b2ddecb37e0c4b082297
Dmitrii Polikarpov <dpolikarpov17@gmail.com>
2026-05-31 00:27:12 +0300

    created a template of main window

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleFile(":/style.qss");
+
+    if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
+        QString qss = QLatin1String(styleFile.readAll());
+        a.setStyleSheet(qss);
+        styleFile.close();
+    } else {
+        qDebug() << "cannot apply stylesheets!";
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,37 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setupUI();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setupUI()
+{
+    setWindowTitle("Инспектор аэропортов");
+    resize(900, 600);
+
+    // main title
+    ui->lblMain->setAlignment(Qt::AlignCenter);
+    ui->lblMain->setObjectName("mainLabel");
+
+    // Left Panel: Table
+    ui->tableSchedule->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableSchedule->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    // Right Panel: Controls
+    ui->rbDeparture->setChecked(true); // Default to departures
+    ui->deDate->setCalendarPopup(true);
+    // Strict date bounds to prevent user error
+    ui->deDate->setDateRange(QDate(2016, 8, 15), QDate(2017, 9, 14));
+    ui->deDate->setDate(QDate(2016, 9, 1));
+
+    // Status Bar
+    lblStatus = new QLabel("Отключено", this);
+    lblStatus->setStyleSheet("color: #EF4444; font-weight: bold; border: none; padding-left: 5px;");
+    statusBar()->addWidget(lblStatus);
 }
 
